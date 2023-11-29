90630ac760b10338407622715b3507f6a0ab6c21
Oxana <oxanal3891@gmail.com>
2023-11-29 02:15:04 +0000

    Tidied up, added prompts, sorted small issues

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 };
@@ -16,70 +16,15 @@ generateBtn.addEventListener('click', writePassword);
 
 alert('Thank you for choosing our random password generator service!\nFor your security, we recomment that you choose a password which:\n1) is minimum 8 characters long,\n2) consists of a mixture of lower and case characters, and numbers, \n3) includes special characters.\nChoose your password parameters>>')
 
-const key_strings = {
-  lowercase: 'abcdefghijklmnopqrstuvwxyz',
-  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  number: '0123456789',
-  symbol: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
-};
-
-function generatePassword() {
-  var passwordCharSet = "";
-
-  var length = window.prompt("Enter a number from 8 to 128 for password length.");
-
-  var lowercase = window.confirm("Would you like to use lowercase letters?");
-  if (lowercase) {
-    passwordCharSet += key_strings.lowercase;
-  };
-
-  var uppercase = window.confirm("Would you like to use uppercase letters?");
-  if (uppercase) {
-    passwordCharSet += key_strings.uppercase;
-  };
-
-  var symbols = window.confirm("Would you like to use symbols?");
-  if (symbols) {
-    passwordCharSet += key_strings.symbol;
-  };
-
-  var numbers = window.confirm("Would you like to use numbers?");
-  if (numbers) {
-    passwordCharSet += key_strings.number;
-  };
-  var password = "";
-  for (let i = 0; i < length; i++) {
-    password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
-  }
-  return password;
-};
-
-/*
-//Global vaariables
-
-var password = "";
-
-var charTypes = "";
-
-//Function to generate password with user input
-function generatePassword() {
-
-  //Declare a user choice array
-
-  for (let i = 0; i < charTypes.length; i++) {
-    password += charTypes[Math.floor(Math.random() * charTypes.length)];
-  };
-
-  return password;
-}
-
-
+//Global variables
+let charTypes = "";
+let howLong;
 
 // Function to prompt user for password options 
-var getPasswordOptions = function () {
+getPasswordOptions = () => {
 
   // Array of special characters to be included in password
-  var specialCharacters = [
+  let specialCharacters = [
     '@',
     '%',
     '+',
@@ -106,10 +51,10 @@ var getPasswordOptions = function () {
   ];
 
   // Array of numeric characters to be included in password
-  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
   // Array of lowercase characters to be included in password
-  var lowerCasedCharacters = [
+  let lowerCasedCharacters = [
     'a',
     'b',
     'c',
@@ -139,7 +84,7 @@ var getPasswordOptions = function () {
   ];
 
   // Array of uppercase characters to be included in password
-  var upperCasedCharacters = [
+  let upperCasedCharacters = [
     'A',
     'B',
     'C',
@@ -170,50 +115,74 @@ var getPasswordOptions = function () {
 
   // Prompts array
 
-  var prompts = [
-    "Please enter the howLong of your password. It must be between 8 and 128 characters long",
+  let prompts = [
+    "Please enter the length of your password. It must be between 8 and 128 characters long",
     "Do you want to include lower case characters?",
     "Do you want to include upper case characters?",
     "Do you want to include numeric characters?",
     "Do you want to include special characters?"
   ];
 
-  //Password howLong var and prompt 
+  //Password length prompt 
 
-  var howLong = window.prompt(prompts[0]);
+  howLong = prompt(prompts[0]);
 
   if (howLong >= 8 && howLong <= 128 && !isNaN(howLong)) {
 
     //Prompt characters and note the number of character types 
 
-    let lowerCharChoice = window.prompt(prompts[1]);
-    if (lowerCharChoice) {
-      charTypes += String(lowerCasedCharacters);
+    let lowerCharChoice = confirm(prompts[1]);
+    if (lowerCharChoice == true) {
+      charTypes += lowerCasedCharacters;
     };
 
-    let upperCharChoice = prompt(prompts[2]);
-    if (upperCharChoice) {
-      charTypes += String(upperCasedCharacters);
+    let upperCharChoice = confirm(prompts[2]);
+    if (upperCharChoice == true) {
+      charTypes += upperCasedCharacters;
     };
 
-    let numberChoice = prompt(prompts[3]);
-    if (numberChoice) {
-      charTypes += String(numericCharacters);
+    let numberChoice = confirm(prompts[3]);
+    if (numberChoice == true) {
+      charTypes += numericCharacters;
     };
 
-    let specialCharChoice = prompt(prompts[4]);
-    if (specialCharChoice) {
-      charTypes += String(specialCharacters);
-    };
+    let specialCharChoice = confirm(prompts[4]);
+    if (specialCharChoice == true) {
+      charTypes += specialCharacters;
+    }
 
     // Check that there is at least 1 character type chosen and ask again if not
 
-    if (!charTypes) {
-      alert("Please select at least one type of character");
+    if (charTypes === "") {
+      alert("Please select at least one type of character. Please choose again.");
       return getPasswordOptions();
-    };
-    return String(charTypes);
+    }
   }
 
-  else { alert("Please type in a valid length") }
+  else {
+    alert("Please type in a valid length");
+    return getPasswordOptions()
+  }
+
+  charTypes = String(charTypes);
+  return charTypes;
+};
+
+//Initialize function
+getPasswordOptions();
+
+//Function to generate password with user input
+generatePassword = () => {
+  let password = "";
+  for (let i = 0; i < howLong; i++) {
+    password += charTypes[Math.floor(Math.random() * charTypes.length)];
+  };
+
+  return password;
 }
+
+//Initialize function
+
+generatePassword();
+
+alert("Thank you! Click on 'Generate Password to get your password :)")
